fbf353ce050d861cb55a8faa5c00bc83bcbbe076
Daniel Ma <drailskid@yahoo.com>
2017-07-08 22:57:52 -0700

    fix tests

--- test/test-helper.el
+++ test/test-helper.el
@@ -2,4 +2,4 @@
 
 (defvar rufo-test/root-path (f-parent (f-parent (f-this-file))))
 
-(require 'rufo-minor-mode (f-join rufo-test/root-path "rufo.el"))
+(require 'rufo (f-join rufo-test/root-path "rufo.el"))
